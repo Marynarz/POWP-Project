c9bc7a4a8e9ebab0b9c28c88ced399b55371535e
Wojtek <nwojtek@gmail.com>
2019-11-18 21:28:34 +0100

    Trace collector. Dodawanie trejsow do pliku

--- src/TraceCollect.py
+++ src/TraceCollect.py
@@ -1,9 +1,17 @@
+from datetime import datetime
 class TraceCollect:
     traceHeader = ""
+    traceFile = ""
     def __init__(self,traceName):
-        with open(traceName+".txt","a") as traceFileHook:
+        self.traceFile = traceName+".txt"
+        with open(self.traceFile,"a") as traceFileHook:
             traceFileHook.write(traceName + " is alive!")
             traceFileHook.close()
         self.traceHeader = traceName
 
-    def 
+    def addTrace(self,traceType,traceMessage):
+        message = datetime.now().time() + " " + self.traceHeader + ": " + traceType + ": " + traceMessage
+        with open(self.traceFile,"a") as traceFileHook:
+            traceFileHook.write(message)
+            traceFileHook.close()
+            
