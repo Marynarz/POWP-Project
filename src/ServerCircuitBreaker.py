77fdb30b5ab9e2b98868440ef7440a89ba247498
206074 <nwojtek@gmail.com>
2020-01-13 19:45:46 +0100

    circuit breaker, ogolny zarys

--- src/ServerCircuitBreaker.py
+++ src/ServerCircuitBreaker.py
@@ -1,7 +1,38 @@
+from src.CircuitBreakerEnum import CircuitBreakerEnum
+
 
 class ServerCircuitBreaker:
     traceCollector = ''
     namePoint = "ServerCircuitCollector"
+    circuitStatus = CircuitBreakerEnum.OPEN
+    dataQueue = []
+
     def __init__(self,traceCollector):
         self.traceCollector = traceCollector
         self.traceCollector.addTrace("INFO",self.namePoint,"SERVER WELCOME!")
+
+    def receiveSignal(self,recSignal):
+        if self.circuitStatus is CircuitBreakerEnum.OPEN:
+            self.dataQueue.append(recSignal)
+        elif (self.circuitStatus is CircuitBreakerEnum.CLOSED) and self.dataQueue:
+            self.dataQueue.append(recSignal)
+        elif (self.circuitStatus is CircuitBreakerEnum.CLOSED) and not self.dataQueue:
+            pass
+
+    def checkStatus(self):
+        if self.sendSignal("Connection test"):
+            self.circuitStatus = CircuitBreakerEnum.CLOSED
+
+    def sendSignal(self,sendSig):
+        pass
+
+    def clearQueue(self):
+        if self.dataQueue and self.circuitStatus is CircuitBreakerEnum.CLOSED:
+            self.sendSignal(self.dataQueue.pop[0])
+            self.checkStatus()
+            self.clearQueue()
+        elif self.dataQueue and self.circuitStatus is CircuitBreakerEnum.OPEN:
+            self.checkStatus()
+            self.clearQueue()
+        elif not self.dataQueue:
+            pass
