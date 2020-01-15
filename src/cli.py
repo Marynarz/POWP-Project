751ce205d1760420aa807b9f8ab1b9032373d94e
wojtek <nwojtek@gmail.com>
2020-01-15 13:14:35 +0100

    help do cli

--- src/cli.py
+++ src/cli.py
@@ -6,6 +6,7 @@ from multiprocessing import Process
 def main():
     processes = {}
     traceObj = TraceCollect("CLI")
+    helpMenu()
     while True:
         x = input('#: ')
         if x is "0":
@@ -19,7 +20,21 @@ def main():
             processes["BankCentral"] = BankCentralObserver(traceObj)
             ports = processes["BankCentral"].getConnEnds()
             processes["BankCentral"].start()
+        elif x is "h":
+            helpMenu()
         else:
             print("Wrong Command!")
+
+def helpMenu():
+    print("---POWP PROJEKT---")
+    print("Robenie przelewow")
+    print("Wykorzystane wzorce: Observer, adapter, cirguit braker")
+    print()
+    print("Komendy CLI:")
+    print("0 - exit")
+    print("1 - start circuit breakera -- nie aktywne")
+    print("2 - start centrali bnak account - observer")
+    print("h - pomoc")
+
 if __name__ == '__main__':
     main()
